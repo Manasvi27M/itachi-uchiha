13a1041e9f0e33509d9e220de3789899d74af7c2
Manasvi M <manasvii.social@gmail.com>
2025-04-24 17:18:12 +0530

    Made the code responsive and added hover effects for the buttons

--- src/components/landingPage/HeroSection.jsx
+++ src/components/landingPage/HeroSection.jsx
@@ -29,12 +29,12 @@ export default function HeroSection() {
         [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"
         >
             <div className="mt-40 mb-8 relative flex items-center justify-center gap-4">
-                <button className="px-8 py-3 bg-white text-gray-850 text-lg font-semibold rounded-md hover:bg-gray-500 transition flex items-center gap-2">
-                    <Sparkles className="h-5 w-5" />
+                <button className="sm:px-8 px-6 py-2 sm:py-3 bg-white text-gray-850 text-base sm:text-lg font-semibold rounded-md duration-300 ease-in-out hover:shadow-[0_10px_20px_rgba(255,255,255,0.7)] hover:bg-white transition flex items-center gap-2">
+                    <Sparkles className="h-4 sm:h-5 w-4 sm:w-5" />
                     Enhance Resume
                 </button>
-                <button className="px-8 py-3 bg-white text-gray-850 text-lg font-semibold rounded-md hover:bg-gray-500 transition flex items-center gap-3">
-                    <Brain className="h-5 w-5 mr-[-8px]" />
+                <button className="sm:px-8 px-6 sm:py-3 py-2 bg-white text-gray-850 text-base sm:text-lg font-semibold rounded-md duration-300 ease-in-out hover:shadow-[0_10px_20px_rgba(255,255,255,0.7)] hover:bg-white transition flex items-center gap-3">
+                    <Brain className="h-4 sm:h-5 w-4 sm:w-5 mr-[-6px] sm:mr-[-8px]" />
                     Generate Resume
                 </button>
             </div>
@@ -44,9 +44,9 @@ export default function HeroSection() {
                 Your Resume With Itachi Uchiha.
             </div>
             
-            <div className="relative w-full mt-16 max-w-6xl mx-auto z-10 perspective-[1000px]">
+            <div className="relative w-full mt-16 px-4 sm:px-6 md:px-8 max-w-6xl mx-auto z-10 perspective-[1000px]">
                 <div
-                    className="h-auto w-full rounded-2xl gap-4 overflow-hidden transition-all"
+                    className="w-full rounded-2xl overflow-hidden shadow-xl transition-all"
                     style={{
                         boxShadow:
                             "rgba(0, 0, 0, 0.3) 0px 0px, rgba(0, 0, 0, 0.29) 0px 9px 20px, rgba(0, 0, 0, 0.26) 0px 37px 37px, rgba(0, 0, 0, 0.15) 0px 84px 50px, rgba(0, 0, 0, 0.04) 0px 149px 60px, rgba(0, 0, 0, 0.01) 0px 233px 65px",
@@ -58,10 +58,10 @@ export default function HeroSection() {
                         alt="hero"
                         draggable="false"
                         loading="lazy"
-                        width="1400"
-                        height="720"
+                        //width="1400"
+                        //height="720"
                         decoding="async"
-                        className="mx-auto rounded-2xl object-cover h-full object-left-top transition-transform duration-300 ease-out"
+                        className="w-full h-auto rounded-2xl object-cover object-top transition-transform duration-300 ease-out"
                         style={{
                             transformOrigin: "bottom center",
                             color: "transparent"
