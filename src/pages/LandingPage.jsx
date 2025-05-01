752751839ddecc889a0213a814e2b4b793550d56
Pranav Keshav <1rn22is047.pranavkeshav@gmail.com>
2025-05-01 23:22:24 +0530

    Added the Features section along with the templates parallax

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,12 @@
 import { div } from "framer-motion/client";
 import HeroSection from "../components/landingPage/HeroSection";
+import FeaturesSection from "../components/LandingPage/FeaturesSection";
 
-
-export default function LandingPage(){
-    return(
-        <div>
-            <HeroSection />
-        </div>
-    );
+export default function LandingPage() {
+  return (
+    <div className="flex flex-col">
+      <HeroSection />
+      <FeaturesSection />
+    </div>
+  );
 }
