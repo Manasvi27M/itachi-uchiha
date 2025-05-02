a6f52c064113ec3b28f8acfcce46d77b5feb308f
Pranav Keshav <1rn22is047.pranavkeshav@gmail.com>
2025-05-02 16:57:33 +0530

    Made changes to the navbar and added the lamp component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 import { div } from "framer-motion/client";
 import HeroSection from "../components/landingPage/HeroSection";
 import FeaturesSection from "../components/LandingPage/FeaturesSection";
+import { LampComponent } from "../components/landingPage/LampEffect";
 
 export default function LandingPage() {
   return (
     <div className="flex flex-col">
       <HeroSection />
       <FeaturesSection />
+      <LampComponent />
     </div>
   );
 }
